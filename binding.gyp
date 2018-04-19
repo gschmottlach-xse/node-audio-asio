cd117428a577361b18a8a785a587e0d8af6ffcad
Glenn Schmottlach <Glenn_Schmottlach@mentor.com>
2018-04-19 13:48:03 -0400

    Automatically pull down ASIO SDK during installation.

--- binding.gyp
+++ binding.gyp
@@ -1,11 +1,25 @@
 {
+	"variables": {
+		"ASIO_SDK":"./thirdparty/ASIOSDK2.3"
+	},
 	"targets": [
 		{
 			"target_name": "nodeAudioAsio",
-			"sources": [ "./host/pc/asiolist.cpp", "./host/asiodrivers.cpp", "./common/asio.cpp", "Source.cpp" ],
-			"include_dirs": [ "asio", "build", "common", "driver", "host", "host/pc", "<!(node -e \"require('nan')\")"],
-		
+			"sources": [
+				"<@(ASIO_SDK)/host/pc/asiolist.cpp",
+				"<@(ASIO_SDK)/host/asiodrivers.cpp",
+				"<@(ASIO_SDK)/common/asio.cpp",
+				"Source.cpp"
+			],
+			"include_dirs": [
+				"<@(ASIO_SDK)/asio",
+				"<@(ASIO_SDK)/build",
+				"<@(ASIO_SDK)/common",
+				"<@(ASIO_SDK)/driver",
+				"<@(ASIO_SDK)/host",
+				"<@(ASIO_SDK)/host/pc",
+				"<!(node -e \"require('nan')\")"
+			],
 		}
-	],
-
+	]
 }
---
+++ tasks/preinstall.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const https = require('https');
+const http = require('http');
+const urlLib = require('url');
+const zlib = require('zlib');
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+
+const asioSdkUrl = 'https://www.steinberg.net/sdk_downloads/asiosdk2.3.zip';
+const asioSdkDir = path.normalize(path.join(__dirname, '..', 'thirdparty','ASIOSDK2.3'));
+const reposPath = path.normalize(path.join(__dirname, '..', 'thirdparty'));
+const zipPath = path.normalize(path.join(reposPath, path.basename(asioSdkUrl)));
+const protocol = { 'http:': http, 'https:': https };
+const topLevel = path.normalize(path.join(__dirname, '..'));
+
+// Install the dependencies need for unzipping an archive
+execSync('npm install -D extract-zip', { cwd: topLevel });
+const extract = require('extract-zip');
+
+function request(url, callback) {
+  let urlObj = urlLib.parse(url);
+  protocol[urlObj.protocol].get(url, (response) => {
+    if ((response.statusCode == 302) || (response.statusCode == 301)) {
+      request(response.headers.location, callback);
+    } else {
+      callback(null, response);
+    };
+  }).on("error", (err) => {
+    callback(err);
+  });
+};
+
+
+if ( !fs.existsSync(asioSdkDir) ) {
+  request(asioSdkUrl, (err, res) => {
+    var error;
+    if ( err ) {
+      error = err;
+    }
+    else if (res.statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${res.statusCode}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    let zipFile = fs.createWriteStream(zipPath, { encoding:'binary'});
+    res.setEncoding("binary");
+    res.once('error', err => {
+      console.error(err);
+    });
+    res.on("data", data => {
+      zipFile.write(data);
+    });
+    res.on("end", () => {
+      zipFile.end(() => {
+        extract(zipPath, { dir: reposPath }, (err) => {
+          if ( err ) {
+            console.error(`${err}`);
+          }
+          else {
+            console.log(`${path.basename(asioSdkUrl)} unzipped`);
+          }
+          // Remove the zipped ASIO SDK
+          fs.unlinkSync(zipPath);
+        });
+      });
+    });
+  });
+}
